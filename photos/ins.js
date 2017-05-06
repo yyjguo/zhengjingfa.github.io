d816f73108ad9576741ebbbdd4461fa1cb92a04c
zhengjingfa <643324224@qq.com>
2017-05-06 19:43:05 +0800

    Site updated: 2017-05-06 19:43:05

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/zhengjingfa/blog-back-up/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/zhengjingfa/blog-back-up/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/zhengjingfa/Blog-Back-Up/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/zhengjingfa/Blog-Back-Up/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
